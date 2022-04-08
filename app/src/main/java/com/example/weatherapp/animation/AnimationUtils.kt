98381fcad4a3ca1a8210054065f95e1ab0edc447
amalnair95 <anair1318@gmail.com>
2022-04-08 18:29:51 +0530

    safety commit

--- app/src/main/java/com/example/weatherapp/animation/AnimationUtils.kt
+++ app/src/main/java/com/example/weatherapp/animation/AnimationUtils.kt
@@ -4,8 +4,6 @@ import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.Transformation
 
-
-
 object AnimationUtils {
     fun expand(view: View) {
         val animation = expandAction(view)
@@ -14,17 +12,17 @@ object AnimationUtils {
 
     private fun expandAction(view: View): Animation {
         view.measure(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        val actualheight = view.measuredHeight
+        val actualHeight = view.measuredHeight
         view.layoutParams.height = 0
         view.visibility = View.VISIBLE
         val animation: Animation = object : Animation() {
             override fun applyTransformation(interpolatedTime: Float, t: Transformation) {
                 view.layoutParams.height =
-                    if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (actualheight * interpolatedTime).toInt()
+                    if (interpolatedTime == 1f) ViewGroup.LayoutParams.WRAP_CONTENT else (actualHeight * interpolatedTime).toInt()
                 view.requestLayout()
             }
         }
-        animation.duration = (actualheight / view.context.resources.displayMetrics.density).toLong()
+        animation.duration = (actualHeight / view.context.resources.displayMetrics.density).toLong()
         animation.duration = 250L
         view.startAnimation(animation)
         return animation
